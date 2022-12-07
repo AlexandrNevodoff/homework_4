ec7d6685843f8d565ef84b5f7fa569f5176cd536
Alexandr Nevodov <nevodoff@gmail.com>
2022-12-07 17:49:08 +0300

    edit

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
 
 int expfunction(int numberA, int numberB)
 {

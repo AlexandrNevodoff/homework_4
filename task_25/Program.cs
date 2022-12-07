39a2c59bc06f72efbfd0b176f5f9b0794cc1fec0
Alexandr Nevodov <nevodoff@gmail.com>
2022-12-07 17:47:39 +0300

    1

---
+++ task_25/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int expfunction(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+  Console.Write("Введите число A:");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B:");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponent = expfunction(numberA, numberB);
+  Console.WriteLine($"Число " + numberA + " в степени " + numberB + " равно: "  + exponent);
